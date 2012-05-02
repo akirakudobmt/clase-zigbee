ea13d12140eb2a2e1abcd1ed11142fe3f4b734a9
edgar.noel2@gmail.com <edgar.noel2@gmail.com@31a313e1-3841-1ee5-9e24-1808828b112b>
2012-05-02 01:16:02 +0000

    Código completo de Práctica #1

--- trunk/Ha OnOffLight/BeeApps/BeeAppinit.c
+++ trunk/Ha OnOffLight/BeeApps/BeeAppinit.c
@@ -541,7 +541,8 @@ extern const poolInfo_t poolInfo[];
 #endif
 
   gAppTaskID = TS_CreateTask(gTsAppTaskPriority_c, BeeAppTask);
- 
+  gTimeTaskID = TS_CreateTask(gTsAppTaskPriority_c, TimeAppTask);
+  
 #ifdef gHostApp_d
   ZtcCPUResetBlackBox();
 #else
@@ -552,7 +553,10 @@ extern const poolInfo_t poolInfo[];
   
 #endif/*__IAR_SYSTEMS_ICC__*/
   
-  
+  /*Init Timer used to count seconds*/
+  gu8SecondsTimerID = TMR_AllocateTimer();    
+  TMR_StartIntervalTimer(gu8SecondsTimerID, 1000, vSecondsTimerCallback);
+      
   /* Start the task scheduler. Does not return. */
   TS_Scheduler();
 
